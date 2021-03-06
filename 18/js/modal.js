7ad33ab969479eef73115c87941fad01fa595744
Keks <keks@htmlacademy.ru>
2021-03-06 11:35:46 +0300

    :heavy_check_mark: Сборка #18

---
+++ 18/js/modal.js
@@ -0,0 +1,57 @@
+const btnContacts = document.querySelector(".btn-contacts");
+const modal = document.querySelector(".modal");
+const modalClose = modal.querySelector(".modal-close");
+const modalForm = modal.querySelector(".modal-form");
+const nameInput = modal.querySelector(".name-input");
+const mailInput = modal.querySelector(".mail-input");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+btnContacts.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal-show");
+
+  if (storage) {
+    nameInput.value = storage;
+    mailInput.focus();
+  } else {
+    nameInput.focus();
+  }
+});
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal-show");
+  modal.classList.remove("modal-error");
+});
+
+modalForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!nameInput.value || !emailInput.value) {
+    evt.preventDefault();
+    modal.classList.remove("modal-error");
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", nameInput.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal-show");
+      modal.classList.remove("modal-error");
+    }
+  }
+});
